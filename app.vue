c516f29a4685d015577ada0690348b02c38fd0ff
sethgabrielbrady <sethgabrielbrady@gmail.com>
2024-08-06 14:07:39 -0400

    Adds some styling to the diplomat slide

--- app.vue
+++ app.vue
@@ -1,6 +1,51 @@
+
+<script>
+  export default {
+    setup() {
+      return {
+        isActive: ref(false),
+      }
+    },
+    methods: {
+      toggleActive() {
+        this.isActive = !this.isActive;
+      }
+    }
+  };
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div
+    @click="toggleActive()"
+    class="landing-page"
+  >
+    <div
+      class="slide-overlay"
+      :class="{ 'slide-active': isActive }"
+    />
+    <DiplomatSlide :isActive="isActive" />
   </div>
 </template>
+
+<style scoped>
+  .landing-page {
+    background-image: url('./assets/Bonvoy.png');
+    background-size: cover;
+    height: 100vh;
+    overflow: hidden;
+    padding: 0;
+    position: relative;
+    transition: background-color 0.5s;
+    width: 100vw;
+  }
+  .slide-overlay {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    background-color: #f20a0a00;
+    transition: background-color 0.5s;
+  }
+  .slide-active {
+    background-color: #000000c4;
+  }
+</style>
---
+++ components/DiplomatSlide.vue
@@ -0,0 +1,34 @@
+
+<script setup>
+  const props = defineProps({
+    isActive: Boolean
+  });
+</script>
+
+<template>
+  <div
+    class="diplomat-slide"
+    :class="{ 'slide-out': !props.isActive, 'slide-in': props.isActive }"
+  >
+    <h1>Diplomat</h1>
+  </div>
+</template>
+
+<style scoped>
+  .diplomat-slide {
+    background-color: #000000;
+    color: aqua;
+    height: 100vh;
+    padding: 3em;
+    position: absolute;
+    top: 0;
+    transition: right 0.5s;
+    width: 55vw;
+  }
+  .slide-out {
+    right: calc(-55vw - 6em);
+  }
+  .slide-in {
+    right: 0;
+  }
+</style>
